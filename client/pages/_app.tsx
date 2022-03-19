bdd7790f22a76a24200fdf8fc43284217112cee7
Facundo Veliz <facundoveliz9@gmail.com>
2022-03-19 15:28:54 -0300

    cleaned client folder structure

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,4 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import React from 'react'
 
 const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
 
