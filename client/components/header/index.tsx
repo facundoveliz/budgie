53ebf09ee9094dec6af0632af0ff04c68687af40
Facundo Veliz <facundoveliz9@gmail.com>
2022-03-22 01:06:15 -0300

    added theme switch support

---
+++ client/components/header/index.tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from 'next'
+import styled from 'styled-components'
+
+const Main = styled.p`
+  display: flex;
+  align-items: center;
+  padding: 12px;
+`
+
+const Button = styled.button`
+  margin-right: 10px;
+  background-color:  ${({ theme }) => theme.body};
+  border: 0;
+  font-size: 30px;
+`
+
+type Props = {
+  toggleTheme: () => void;
+  isDarkTheme: boolean;
+}
+
+const Header: NextPage<Props> = ({toggleTheme, isDarkTheme}: Props) => {
+  return (
+    <Main>
+      <Button onClick={toggleTheme} type="button">
+        {isDarkTheme ? (
+          <span aria-label="Light mode" role="img">
+            🌕
+          </span>
+        ) : (
+          <span aria-label="Dark mode" role="img">
+            🌞
+          </span>
+        )}
+      </Button>
+      <h1>Header</h1>
+    </Main>
+  )
+}
+
+export default Header
---
+++ client/themes/index.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const lightTheme = {
+  body: '#f1f1f1',
+  text: '#121620',
+}
+
+export const darkTheme = {
+  body: '#121620',
+  text: '#f1f1f1',
+}
+
+export type ThemeType = {
+  theme: typeof darkTheme
+}
+
+export const GlobalStyle = createGlobalStyle<ThemeType>`
+  body {
+    margin: 0;
+    padding: 0;
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+    // transition: background 0.2s ease-in, color 0.2s ease-in;
+  }
+`
