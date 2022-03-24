0e93e5e4ac304c58dad9830da8de542be146b299
Facundo <facundoveliz9@gmail.com>
2022-03-23 22:44:39 -0300

    🔁 changed format of eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'consistent-return': 'off',
+    'no-underscore-dangle': 'off',
+    semi: [
+      'error',
+      'never',
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
+    '@typescript-eslint/no-namespace': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+}
