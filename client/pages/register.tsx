ae11d913797a19861d91c8c79bbf33685d21a52b
Facundo <facundoveliz9@gmail.com>
2022-03-27 19:38:24 -0300

    📓 added login and register

--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,70 +1,123 @@
-import React from 'react';
+import React, { ReactElement, useEffect } from 'react';
 import type { NextPage } from 'next';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import Link from 'next/link';
 
 import { registerUser } from '../api/users';
+import { Button } from '../components/styles/Button';
+import * as F from '../components/styles/Form';
 
-interface IFormInputs {
-  name: string
-  email: string
-  password: string
-  passwordConfirm: string
-}
+type IFormInputs = {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+};
 
-const schema = yup.object({
-  name: yup
-    .string()
-    .required('The name is a required field.')
-    .min(3, 'The name should be at least 3 characters.')
-    .max(128, 'The name should not have more than 128 characters.'),
-  email: yup
-    .string()
-    .required('The email is a required field.')
-    .email('Email must be a valid email.'),
-  password: yup
-    .string()
-    .required('The password is a required field.')
-    .min(8, 'The password should be at least 8 characters.')
-    .max(128, 'The password should not have more than 128 characters.'),
-  passwordConfirm: yup
-    .string()
-    .required('The password is a required field.')
-    .min(8, 'The password should be at least 8 characters.')
-    .max(128, 'The password should not have more than 128 characters.')
-    .oneOf([yup.ref('password'), null], 'Passwords must match.'),
+type RegisterType = NextPage & { getLayout: any };
 
+const schema = yup
+  .object({
+    name: yup
+      .string()
+      .required('The name is a required field.')
+      .min(3, 'The name should be at least 3 characters.')
+      .max(128, 'The name should not have more than 128 characters.'),
+    email: yup
+      .string()
+      .required('The email is a required field.')
+      .email('Email must be a valid email.'),
+    password: yup
+      .string()
+      .required('The password is a required field.')
+      .min(8, 'The password should be at least 8 characters.')
+      .max(128, 'The password should not have more than 128 characters.'),
+    passwordConfirm: yup
+      .string()
+      .required('The password is a required field.')
+      .min(8, 'The password should be at least 8 characters.')
+      .max(128, 'The password should not have more than 128 characters.')
+      .oneOf([yup.ref('password'), null], 'Passwords must match.'),
+  })
+  .required();
 
-}).required();
+const Register: RegisterType = function Register() {
+  useEffect(() => {
+    if (localStorage.getItem('x-auth-token')) {
+      window.location.href = '/';
+    }
+  }, []);
 
-const Register: NextPage = function Register() {
-  const { register, handleSubmit, formState: { errors } } = useForm<IFormInputs>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInputs>({
     resolver: yupResolver(schema),
   });
 
   const onSubmit = (data: IFormInputs) => registerUser(data);
 
   return (
-    <>
+    <F.Wrapper>
       <h1>Register</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('name')} />
-        <p>{errors.name?.message}</p>
+      <F.Form onSubmit={handleSubmit(onSubmit)}>
+        <F.InputWrapper>
+          <F.Label>Username</F.Label>
+          {errors.name ? (
+            <F.Input error {...register('name')} />
+          ) : (
+            <F.Input {...register('name')} />
+          )}
+          <p>{errors.name?.message}</p>
+        </F.InputWrapper>
 
-        <input {...register('email')} />
-        <p>{errors.email?.message}</p>
+        <F.InputWrapper>
+          <F.Label>Email Adress</F.Label>
+          {errors.email ? (
+            <F.Input error {...register('email')} />
+          ) : (
+            <F.Input {...register('email')} />
+          )}
+          <p>{errors.email?.message}</p>
+        </F.InputWrapper>
 
-        <input {...register('password')} />
-        <p>{errors.password?.message}</p>
+        <F.InputWrapper>
+          <F.Label>Password</F.Label>
+          {errors.password ? (
+            <F.Input error {...register('password')} />
+          ) : (
+            <F.Input {...register('password')} />
+          )}
+          <p>{errors.password?.message}</p>
+        </F.InputWrapper>
 
-        <input {...register('passwordConfirm')} />
-        <p>{errors.passwordConfirm?.message}</p>
+        <F.InputWrapper>
+          <F.Label>Confirm password</F.Label>
+          {errors.passwordConfirm ? (
+            <F.Input error {...register('passwordConfirm')} />
+          ) : (
+            <F.Input {...register('passwordConfirm')} />
+          )}
+          <p>{errors.passwordConfirm?.message}</p>
+        </F.InputWrapper>
 
-        <button type="submit">Submit</button>
-       </form>
-    </>
+        <F.SubmitWrapper>
+          <Button type="submit">Register</Button>
+          <Link passHref href="/login">
+            <p>Create an account</p>
+          </Link>
+        </F.SubmitWrapper>
+      </F.Form>
+    </F.Wrapper>
   );
+};
+
+// custom layout without header/footer
+Register.getLayout = function PageLayout(page: ReactElement) {
+  return <>{page}</>;
 };
 
 export default Register;
