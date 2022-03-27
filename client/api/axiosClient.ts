44736df0fe5f450cc07dda969509f4d228a7118e
Facundo <facundoveliz9@gmail.com>
2022-03-27 19:35:28 -0300

    🔧 improved error handling

--- client/api/axiosClient.ts
+++ client/api/axiosClient.ts
@@ -9,17 +9,24 @@ axiosClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('x-auth-token');
     if (token) {
-      config.headers!.Authorization = `Bearer ${token }`;
-      console.log('token exists');
+      config.headers!.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => Promise.reject(error),
+  (error) => console.log(error),
 );
 
 axiosClient.interceptors.response.use(
   (response) => response,
-  (error) => Promise.reject(error),
+  (error) => {
+    if (error.response.status === 401) {
+      return (window.location.href = '/login');
+    }
+    if (error.response.data.msg === 'Invalid email or password') {
+      return console.log(error.response.data.msg);
+    }
+    return console.log(error);
+  },
 );
 
 export default axiosClient;
