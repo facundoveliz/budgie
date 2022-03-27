8968a27ff8b4652ec719cd7004b656d22790b2c2
Facundo <facundoveliz9@gmail.com>
2022-03-27 19:38:55 -0300

    💅 improved theme switching and layouts

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
-import type { AppProps } from 'next/app';
+import React, { ReactElement, ReactNode } from 'react';
+import { AppProps } from 'next/app';
+import Layout from '../components/layout';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, lightTheme } from '../themes/index';
+import { NextPage } from 'next';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  // @ts-ignore, next-js stuff
-  return <Component pageProps={pageProps} />;
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // custom layout without header/footer for auth pages
+  if (Component.getLayout) {
+    return Component.getLayout(
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>,
+    );
+  }
+
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,37 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme, GlobalStyle } from '../themes/index';
+import { getEntries } from '../api/entries';
 
-import Header from '../components/header/index';
+type Entry = {
+  _id: string,
+  category: string,
+  income: boolean,
+  amount: number,
+};
 
 const Home: NextPage = function Home() {
-  const [theme, setTheme] = useState('light');
-  const isDarkTheme = theme === 'dark';
+  const [entries, setEntries] = useState<Entry[]>();
+  const [loading, setLoading] = useState<boolean>(true);
 
-  const toggleTheme = () => {
-    const updatedTheme = isDarkTheme ? 'light' : 'dark';
-    setTheme(updatedTheme);
-    localStorage.setItem('theme', updatedTheme);
+  const getEntryRequest = async () => {
+    let res = await getEntries();
+    if (res) {
+      setEntries(res.data.result);
+      console.log(res);
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem('theme');
-    const prefersDark = window.matchMedia
-      && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    if (savedTheme && ['dark', 'light'].includes(savedTheme)) {
-      setTheme(savedTheme);
-    } else if (prefersDark) {
-      setTheme('dark');
-    }
+    getEntryRequest();
   }, []);
 
   return (
-    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
-      <GlobalStyle />
-      <Header toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} />
-      <p>Index</p>
-    </ThemeProvider>
+    <>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        entries?.map((entry) => (
+            <div key={entry._id}>
+                <p>{entry.category}</p>
+                <p>{entry.amount}</p>
+            </div>
+        ))
+      )}
+    </>
   );
 };
+
 export default Home;
