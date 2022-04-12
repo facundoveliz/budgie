623f78be009ef6aa7f007c4e43f130e2fbd4213a
Facundo <facundoveliz9@gmail.com>
2022-04-12 18:24:50 -0300

    🔧 heroku changes

--- server.ts
+++ server.ts
@@ -3,7 +3,6 @@ import express from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
 import cors from 'cors'
-import path from 'path'
 
 import routes from './routes'
 import { errorHandler, notFound } from './middleware/errorHandlerMiddleware'
@@ -15,12 +14,7 @@ dotenv.config()
 app.use(express.json())
 app.use(cors({ credentials: true }))
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, 'client/.next')))
 app.use(routes)
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/.next/server/pages/index.html'))
-})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
