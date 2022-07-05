14c54e35402e69fdf43a8d6ff8fd7700c8044e76
Facundo <facundoveliz9@gmail.com>
2022-07-05 16:59:52 -0300

    💰 changed favicon and title

--- client/components/layout.tsx
+++ client/components/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React, { ReactNode } from 'react';
 import Footer from './footer';
 import Header from './header';
@@ -9,6 +10,14 @@ type Children = {
 export default function Layout({ children }: Children) {
   return (
     <>
+      <Head>
+        <title>Personal Budget</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>💰</text></svg>"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
       {children}
       <Footer />
