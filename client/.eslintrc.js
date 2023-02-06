efa6aacdb1beedf7f0afe837a015cf585c5198ce
Facundo Veliz <facundoveliz9@gmail.com>
2023-02-06 20:16:48 -0300

    disabled "@typescript-eslint/no-use-before-define" and changed " to '

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -5,34 +5,38 @@ module.exports = {
   },
   root: true,
   extends: [
-    "plugin:react/recommended",
-    "airbnb-typescript",
-    "plugin:@next/next/recommended",
-    "plugin:import/recommended",
+    'plugin:react/recommended',
+    'airbnb-typescript',
+    'plugin:@next/next/recommended',
+    'plugin:import/recommended',
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react", "@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
   parserOptions: {
-    project: "tsconfig.json",
+    project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   rules: {
-    // disable error jsx not allowed in tsx
-    "react/jsx-filename-extension": [0],
-    // disable extensions in imports because ts conflict
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    // In some files I use styled-components directly in the
+    // file, the best place to put it is in the end of the file,
+    // and this error conflicts with that.
+    '@typescript-eslint/no-use-before-define': [0],
+    // Disable error jsx not allowed in tsx
+    'react/jsx-filename-extension': [0],
+    // Disable extensions in imports because ts conflict
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
     ],
   },
