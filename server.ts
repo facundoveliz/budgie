345c704846834fb33787357b9e1a5cf0487745fb
Facundo Veliz <facundoveliz9@gmail.com>
2023-05-17 16:05:20 -0300

    updated port listener

--- server.ts
+++ server.ts
@@ -16,7 +16,7 @@ app.use(cors({ credentials: true }))
 app.use(morgan('dev'))
 app.use(routes)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Listening on port ${port}...`))
 
 mongoose
