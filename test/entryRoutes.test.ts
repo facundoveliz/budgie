014ada7ce3e3b5e09b3ca3e6b644397ab38af111
Facundo Veliz <facundoveliz9@gmail.com>
2024-02-16 18:00:47 -0300

    WIP modified and added test for entryRoutes

--- test/entryRoutes.test.ts
+++ test/entryRoutes.test.ts
@@ -25,84 +25,99 @@ afterAll(async () => {
 let token: string
 let id: string
 
-describe('POST /api/users/register and /api/users/login to get a jwtToken', () => {
+describe('User Registrarion and Authentication', () => {
   it('should register a new user', async () => {
-    await request(app)
+    const res = await request(app)
       .post('/api/users/register')
       .send({
         name: 'John Doe',
         email: 'johndoe@gmail.com',
         password: 'johnDoePassword',
       })
-      .expect(200)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
   })
 
   it('should login the user', async () => {
-    await request(app)
+    const res = await request(app)
       .post('/api/users/login')
       .send({
         email: 'johndoe@gmail.com',
         password: 'johnDoePassword',
       })
-      .expect(200)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-        token = res.body.result
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
+    token = res.body.result
   })
 })
 
-describe('POST /api/entries', () => {
-  it('should post a new entry', async () => {
-    await request(app)
+describe('Entry Creation', () => {
+  it('should create a new entry', async () => {
+    const res = await request(app)
       .post('/api/entries')
       .send({
         category: 'Savings',
         type: true,
         amount: 420,
       })
-      .expect(200)
       .set({ Authorization: `Bearer ${token}` })
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-        id = res.body.result._id
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
+    expect(res.body.msg).toEqual("Entry created");
+    id = res.body.result._id
   })
 
-  it('should throw validation error', async () => {
-    await request(app)
+  it('should throw validation error because has missing data', async () => {
+    const res = await request(app)
       .post('/api/entries')
       .send({
         category: '',
         type: true,
         amount: 420,
       })
-      .expect(400)
       .set({ Authorization: `Bearer ${token}` })
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false)
+
+    expect(400)
+    expect(res.body.ok).toBe(false)
+    expect(res.body.msg).toEqual("Validation error");
+  })
+
+  it('should throw error because the jwt token is invalid', async () => {
+    const res = await request(app)
+      .post('/api/entries')
+      .send({
+        category: '',
+        type: true,
+        amount: 420,
       })
-      .catch((err) => {
-        throw err
+      .set({ Authorization: `Bearer ${token}+1` })
+
+    expect(401)
+    expect(res.body.ok).toBe(false)
+    expect(res.body.msg).toEqual("Invalid token");
+  })
+
+  it('should throw error because the jwt token doesn\'t exist', async () => {
+    const res = await request(app)
+      .post('/api/entries')
+      .send({
+        category: '',
+        type: true,
+        amount: 420,
       })
+
+    expect(401)
+    expect(res.body.ok).toBe(false)
+    expect(res.body.msg).toEqual("No token");
   })
 })
 
 describe('PUT /api/entries/:id', () => {
   it('should edit an entry', async () => {
-    await request(app)
+    const res = await request(app)
       .put(`/api/entries/${id}`)
       .send({
         category: 'Salary',
@@ -110,59 +125,87 @@ describe('PUT /api/entries/:id', () => {
         amount: 5000,
         oldAmount: 420,
       })
-      .expect(200)
       .set({ Authorization: `Bearer ${token}` })
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
   })
 
-  it('should throw validation error', async () => {
-    await request(app)
+  it('should throw validation error because has missing data', async () => {
+    const res = await request(app)
       .put(`/api/entries/${id}`)
       .send({
         category: '',
         type: true,
         amount: 420,
       })
-      .expect(400)
       .set({ Authorization: `Bearer ${token}` })
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false)
+
+    expect(400)
+    expect(res.body.ok).toBe(false)
+  })
+
+  it('should throw validation error because has missing data', async () => {
+    const res = await request(app)
+      .put(`/api/entries/${id}`)
+      .send({
+        category: '',
+        type: true,
+        amount: 420,
       })
-      .catch((err) => {
-        throw err
+      .set({ Authorization: `Bearer ${token}` })
+
+    expect(400)
+    expect(res.body.ok).toBe(false)
+    expect(res.body.msg).toEqual("Validation error");
+  })
+
+  it('should throw error because the jwt token is invalid', async () => {
+    const res = await request(app)
+      .put(`/api/entries/${id}`)
+      .send({
+        category: '',
+        type: true,
+        amount: 420,
       })
+      .set({ Authorization: `Bearer ${token}+1` })
+
+    expect(401)
+    expect(res.body.ok).toBe(false)
+    expect(res.body.msg).toEqual("Invalid token");
+  })
+
+  it('should throw error because the jwt token doesn\'t exist', async () => {
+    const res = await request(app)
+      .put(`/api/entries/${id}`)
+      .send({
+        category: '',
+        type: true,
+        amount: 420,
+      })
+
+    expect(401)
+    expect(res.body.ok).toBe(false)
+    expect(res.body.msg).toEqual("No token");
   })
 })
 
 describe('DELETE /api/entries/:id', () => {
   it('should delete an entry', async () => {
-    await request(app)
+    const res = await request(app)
       .delete(`/api/entries/${id}`)
-      .expect(200)
       .set({ Authorization: `Bearer ${token}` })
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
   })
 
   it('should throw bad request error', async () => {
-    await request(app)
+    const res = await request(app)
       .delete('/api/entries/62478cd6f33256ab42165e1z')
-      .expect(400)
       .set({ Authorization: `Bearer ${token}` })
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false)
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(400)
+    expect(res.body.ok).toBe(false)
   })
 })
