f0517018a7b8952de654d5d56a942aebe62d0e38
Facundo Veliz <facundoveliz9@gmail.com>
2024-02-16 19:35:04 -0300

    code cleaning

--- test/userRoutes.test.ts
+++ test/userRoutes.test.ts
@@ -22,340 +22,261 @@ afterAll(async () => {
 
 let token: string
 
-describe('POST /api/users/register', () => {
+describe('User Registration', () => {
   it('should register a new user', async () => {
-    await request(app)
+    const res = await request(app)
       .post('/api/users/register')
       .send({
         name: 'John Doe',
         email: 'johndoe@gmail.com',
         password: 'johnDoePassword',
       })
-      .expect(200)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-        expect(res.body.msg).toMatch("User created");
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
+    expect(res.body.msg).toEqual("User created");
   })
 
   it('should throw error because the email already exists', async () => {
-    await request(app)
+    const res = await request(app)
       .post('/api/users/register')
       .send({
         name: 'John Doe',
         email: 'johndoe@gmail.com',
         password: 'johnDoePassword',
-      }).expect(400)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false);
-        expect(res.body.msg).toMatch("Invalid email or password");
       })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(400)
+    expect(res.body.ok).toBe(false);
+    expect(res.body.msg).toEqual("Invalid email or password");
 
   });
 
   it('should throw error because the email format is invalid', async () => {
-    await request(app)
+    const res = await request(app)
       .post('/api/users/register')
       .send({
         name: 'John Doe',
         email: 'johndoegmail.com',
         password: 'johnDoePassword',
-      }).expect(400)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false);
-        expect(res.body.msg).toMatch("Validation error");
       })
-      .catch((err) => {
-        throw err
-      })
+    expect(400)
+    expect(res.body.ok).toBe(false);
+    expect(res.body.msg).toEqual("Validation error");
   });
 
   it('should throw error because the name is empty', async () => {
-    await request(app)
+    const res = await request(app)
       .post('/api/users/register')
       .send({
         name: '',
         email: 'johndoe@gmail.com',
         password: 'johnDoePassword',
-      }).expect(400)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false);
-        expect(res.body.msg).toMatch("Validation error");
       })
-      .catch((err) => {
-        throw err
-      })
+    expect(400)
+    expect(res.body.ok).toBe(false);
+    expect(res.body.msg).toEqual("Validation error");
   });
 
   it('should throw error because the password is too short', async () => {
-    await request(app)
+    const res = await request(app)
       .post('/api/users/register')
       .send({
         name: 'John Doe',
         email: 'johndoe@gmail.com',
         password: 'johnDoe',
-      }).expect(400)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false);
-        expect(res.body.msg).toMatch("Validation error");
       })
-      .catch((err) => {
-        throw err
-      })
+    expect(400)
+    expect(res.body.ok).toBe(false);
+    expect(res.body.msg).toEqual("Validation error");
   });
 })
 
-describe('POST /api/users/login', () => {
+describe('User Login', () => {
   it('should login the user', async () => {
-    await request(app)
+    const res = await request(app)
       .post('/api/users/login')
       .send({
         email: 'johndoe@gmail.com',
         password: 'johnDoePassword',
       })
-      .expect(200)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-        expect(res.body.msg).toMatch("User logged");
-        token = res.body.result
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
+    expect(res.body.msg).toEqual("User logged");
+    token = res.body.result
   })
 
   it('should throw error because the email is invalid', async () => {
-    await request(app)
+    const res = await request(app)
       .post('/api/users/login')
       .send({
         email: 'johndoewrong@gmail.com',
         password: 'johnDoePassword',
       })
-      .expect(400)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false)
-        expect(res.body.msg).toMatch("Invalid email or password");
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(400)
+    expect(res.body.ok).toBe(false)
+    expect(res.body.msg).toEqual("Invalid email or password");
   })
 
   it('should throw error because the password is incorrect', async () => {
-    await request(app)
+    const res = await request(app)
       .post('/api/users/login')
       .send({
         email: 'johndoe@gmail.com',
         password: 'johnDoeWrongPassword',
       })
-      .expect(400)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false)
-        expect(res.body.msg).toMatch("Invalid email or password");
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(400)
+    expect(res.body.ok).toBe(false)
+    expect(res.body.msg).toEqual("Invalid email or password");
   })
 })
 
-describe('GET /api/user', () => {
-  it('should get current user info', async () => {
-    await request(app)
+describe('Get Current User', () => {
+  it('should retrieve current user info', async () => {
+    const res = await request(app)
       .get('/api/users')
-      .expect(200)
       .set({ Authorization: `Bearer ${token}` })
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-        expect(res.body.msg).toMatch("User founded");
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
+    expect(res.body.msg).toEqual("User founded");
   })
 
   it('should throw error because the jwt token is invalid', async () => {
-    await request(app)
+    const res = await request(app)
       .get('/api/users')
-      .expect(401)
       .set({ Authorization: `Bearer ${token}+1` })
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false)
-        expect(res.body.msg).toMatch("Invalid token");
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(401)
+    expect(res.body.ok).toBe(false)
+    expect(res.body.msg).toEqual("Invalid token");
   })
 
   it('should throw error because the jwt token doesn\'t exist', async () => {
-    await request(app)
+    const res = await request(app)
       .get('/api/users')
-      .expect(401)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false)
-        expect(res.body.msg).toMatch("No token");
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(401)
+    expect(res.body.ok).toBe(false)
+    expect(res.body.msg).toEqual("No token");
   })
 })
 
-describe('PUT /api/users/', () => {
+describe('Update User', () => {
   it('should update the user name', async () => {
-    await request(app)
+    const res = await request(app)
       .put('/api/users')
       .set({ Authorization: `Bearer ${token}` })
       .send({
         name: 'Jane Doe',
       })
-      .expect(200)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-        expect(res.body.msg).toMatch("User updated");
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
+    expect(res.body.msg).toEqual("User updated");
   })
 
   it('should update the email', async () => {
-    await request(app)
+    const res = await request(app)
       .put('/api/users')
       .set({ Authorization: `Bearer ${token}` })
       .send({
         email: 'janedoe@gmail.com',
       })
-      .expect(200)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-        expect(res.body.msg).toMatch("User updated");
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
+    expect(res.body.msg).toEqual("User updated");
   })
 
   it('should update the pasword', async () => {
-    await request(app)
+    const res = await request(app)
       .put('/api/users')
       .set({ Authorization: `Bearer ${token}` })
       .send({
         password: 'janeDoePassword',
       })
-      .expect(200)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-        expect(res.body.msg).toMatch("User updated");
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
+    expect(res.body.msg).toEqual("User updated");
   })
 
   // create user for email checking
   it('should register a new user', async () => {
-    await request(app)
+    const res = await request(app)
       .post('/api/users/register')
       .send({
         name: 'Jane Doe',
         email: 'janedoe@mail.com',
         password: 'janeDoePassword',
       })
-      .expect(200)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-        expect(res.body.msg).toMatch("User created");
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
+    expect(res.body.msg).toEqual("User created");
   })
 
   it('should throw error because the email already exists', async () => {
-    await request(app)
+    const res = await request(app)
       .put('/api/users')
       .set({ Authorization: `Bearer ${token}` })
       .send({
         email: 'janedoe@mail.com',
       })
-      .expect(400)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false)
-        expect(res.body.msg).toMatch("Invalid email or password");
-        token = res.body.result
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(400)
+    expect(res.body.ok).toBe(false)
+    expect(res.body.msg).toEqual("Invalid email or password");
+    token = res.body.result
   })
 })
 
-describe('DELETE /api/users/', () => {
+describe('Delete User Account', () => {
   // login so it will load the token again
   it('should login the user', async () => {
-    await request(app)
+    const res = await request(app)
       .post('/api/users/login')
       .send({
         email: 'janedoe@gmail.com',
         password: 'janeDoePassword',
       })
-      .expect(200)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-        expect(res.body.msg).toMatch("User logged");
-        token = res.body.result
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
+    expect(res.body.msg).toEqual("User logged");
+    token = res.body.result
   })
 
   it('should delete the user', async () => {
-    await request(app)
+    const res = await request(app)
       .delete('/api/users')
       .set({ Authorization: `Bearer ${token}` })
-      .expect(200)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', true)
-        expect(res.body.msg).toMatch("User deleted");
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(200)
+    expect(res.body.ok).toBe(true)
+    expect(res.body.msg).toEqual("User deleted");
   })
 
   it('should throw error because the jwt token doesn\'t exist', async () => {
-    await request(app)
+    const res = await request(app)
       .delete('/api/users')
-      .expect(401)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false)
-        expect(res.body.msg).toMatch("No token");
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(401)
+    expect(res.body.ok).toBe(false)
+    expect(res.body.msg).toEqual("No token");
   })
 
   it('should throw error because the jwt token is invalid', async () => {
-    await request(app)
+    const res = await request(app)
       .delete('/api/users')
       .set({ Authorization: `Bearer ${token}+1` })
-      .expect(401)
-      .then(async (res) => {
-        expect(res.body).toHaveProperty('ok', false)
-        expect(res.body.msg).toMatch("Invalid token");
-      })
-      .catch((err) => {
-        throw err
-      })
+
+    expect(401)
+    expect(res.body.ok).toBe(false)
+    expect(res.body.msg).toEqual("Invalid token");
   })
 })
